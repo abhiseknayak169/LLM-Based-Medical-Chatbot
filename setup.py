f7abefcdd4d8719e7411c88774785c893a840470
Abhisek Nayak <abhiseknayak169@gmail.com>
2025-05-25 16:26:26 +0530

    New Project Created and Folder Structure Added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Medical Chatbot',
+    version= '0.0.0',
+    author= 'Abhisek Nayak',
+    author_email= 'abhiseknayak169@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
